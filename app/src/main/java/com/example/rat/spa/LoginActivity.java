ce170f12bc53538305788d651b2e764df1da1442
Phan Huy Hoàng <snailfalca@gmail.com>
2018-10-25 10:10:51 +0700

    Apply logics to Login UI...!
    - Handle user login if saved token is invalid.
    - Save username & password to SharedPreferences.

--- app/src/main/java/com/example/rat/spa/LoginActivity.java
+++ app/src/main/java/com/example/rat/spa/LoginActivity.java
@@ -6,7 +6,8 @@ import android.content.SharedPreferences;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.VolleyError;
@@ -17,39 +18,37 @@ import org.json.JSONObject;
 import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity {
+  private EditText etUsername;
+  private EditText etPassword;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
 
+    initViewVariables();
+
     String savedToken = getSavedToken();
-    String url = SpaUtil.URL.USER_LOGIN;
-    HashMap<String, String> params = new HashMap<>();
-    boolean updateToken = true;
 
     if (savedToken != null) {
-      updateToken = false;
-      url = SpaUtil.URL.USER_INDEX;
-      params.put("Token", savedToken);
+      handleSavedToken(savedToken);
     } else {
-      params.put("Username", "admin_cloud");
-      params.put("Password", "123123");
+      initLogin();
     }
+  }
 
-    final boolean finalUpdateToken = updateToken;
+  private void initViewVariables() {
+    etUsername = findViewById(R.id.et_username);
+    etPassword = findViewById(R.id.et_password);
+  }
+
+  private void handleSavedToken(String token) {
+    String url = SpaUtil.URL.USER_INDEX;
+    HashMap<String, String> params = new HashMap<>();
+    params.put("Token", token);
     new RequestHandler(this) {
       @Override
       public void handleResult(String result) {
-        if (finalUpdateToken) {
-          try {
-            saveUserInfo(result);
-          } catch (Exception e) {
-            e.printStackTrace();
-            return;
-          }
-        }
-
         Intent intent = new Intent(getApplicationContext(), UserInfoActivity.class);
         intent.putExtra("json-user-info", result);
         startActivity(intent);
@@ -58,21 +57,18 @@ public class LoginActivity extends AppCompatActivity {
 
       @Override
       public void handleError(VolleyError error) {
-        Toast.makeText(LoginActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+        initLogin();
       }
     }.POST(url, params);
   }
 
-  private void saveUserInfo(String jsonStr) throws Exception{
-    try {
-      JSONObject json = new JSONObject(jsonStr);
-      if (json.getInt("Status") != 1) throw new Exception("Request failed...!");
-      String token = json.getJSONObject("Data").getString("Token");
-      saveToken(token);
-    } catch (JSONException e) {
-      e.printStackTrace();
-      throw new Exception("Parse JSON failed...!");
-    }
+  private void initLogin() {
+    SharedPreferences prefs = getSharedPreferences("spa", Context.MODE_PRIVATE);
+    String username = prefs.getString("username", "");
+    String password = prefs.getString("password", "");
+
+    etUsername.setText(username);
+    etPassword.setText(password);
   }
 
   @Nullable
@@ -81,8 +77,52 @@ public class LoginActivity extends AppCompatActivity {
     return prefs.getString("token", null);
   }
 
-  private void saveToken(String token) {
+  private void saveLoginDetail(String token) {
+    String username = etUsername.getText().toString();
+    String password = etPassword.getText().toString();
     SharedPreferences prefs = getSharedPreferences("spa", Context.MODE_PRIVATE);
     prefs.edit().putString("token", token).apply();
+    prefs.edit().putString("username", username).apply();
+    prefs.edit().putString("password", password).apply();
+  }
+
+  public void attemptLogin(View view) {
+    String username = etUsername.getText().toString();
+    String password = etPassword.getText().toString();
+    String url = SpaUtil.URL.USER_LOGIN;
+    HashMap<String, String> params = new HashMap<>();
+    params.put("Username", username);
+    params.put("Password", password);
+    new RequestHandler(this) {
+      @Override
+      public void handleResult(String json) {
+        handleLoginResult(json);
+      }
+
+      @Override
+      public void handleError(VolleyError error) {
+        Toast.makeText(LoginActivity.this, "Login failed...!", Toast.LENGTH_SHORT).show();
+      }
+    }.POST(url, params);
+
+  }
+
+  private void handleLoginResult(String json) {
+    try {
+      JSONObject result = new JSONObject(json);
+      int status = result.getInt("Status");
+      if (status != 1) throw new Exception();
+
+      Intent intent = new Intent(getApplicationContext(), UserInfoActivity.class);
+      intent.putExtra("json-user-info", json);
+      startActivity(intent);
+
+      String token = result.getJSONObject("Data").getString("Token");
+      saveLoginDetail(token);
+      finish();
+    } catch (Exception e) {
+      e.printStackTrace();
+      Toast.makeText(this, "Login failed...!", Toast.LENGTH_SHORT).show();
+    }
   }
 }
